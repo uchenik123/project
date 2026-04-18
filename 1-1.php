2ef1ab2f21d1352a85fffa80e03e670dab2a632a
Test <test@test.ru>
2026-04-18 11:12:25 +0300

    V2

---
+++ 1-1.php
@@ -0,0 +1,13 @@
+<?php
+$a = 10;
+$b = 3.14;
+$c = "Текст";
+$d = true;
+$e = [1, 2];
+
+echo gettype($a) . "<br>";
+echo gettype($b) . "<br>";
+echo gettype($c) . "<br>";
+echo gettype($d) . "<br>";
+echo gettype($e) . "<br>";
+?>
